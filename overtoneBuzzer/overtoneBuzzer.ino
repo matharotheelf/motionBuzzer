ba7c9fb7a1e76327cd8ed165d6bdaa4f644ea6bb
thomas lawrence <thomaslawrence@thomass-mbp.eduroam.student>
2023-10-24 16:39:45 +0100

    Add comments to code

--- overtoneBuzzer/overtoneBuzzer.ino
+++ overtoneBuzzer/overtoneBuzzer.ino
@@ -1,21 +1,33 @@
+// LEDs to show timer duration
 int led1Pin = 10;
 int led2Pin = 11;
 int led3Pin = 12;
 int led4Pin = 13;
+// Buzzer
 int buzzerPin = 3;
+// Motion detector which triggers timer
 int motionDetectorPin = 2;
+
+// Start time of hand motions
 unsigned long int motionStartTime;
+// Duration of time between tqo hand motions
 unsigned long int motionDuration;
+
+// Value of motion detector reading in current and previous loop iterations
 int motionDetectorValuePrevious;
 int motionDetectorValueCurrent;
+
+// Start time of loop iteration of Arduino
 unsigned long int loopStartTime;
+
+// Start time of LED light cycle
 unsigned long int cycleStartTime;
 
   
 void setup() 
   
 {
-  Serial.begin(9600);
+  // Register Input and output pins
   pinMode(motionDetectorPin, INPUT);
   pinMode(buzzerPin, OUTPUT);
   pinMode(led1Pin, OUTPUT);
@@ -29,11 +41,14 @@ void setup()
 void loop()
   
 {
+  // Register loop time
   loopStartTime = millis();
-  
+
+  // Read motion detector values
   motionDetectorValuePrevious = motionDetectorValueCurrent;
   motionDetectorValueCurrent = digitalRead(motionDetectorPin);
 
+  // Logic for turning on and off lights to display duration
   if(cycleStartTime != 0) {    
     if ((loopStartTime - cycleStartTime) > motionDuration) {
       digitalWrite(led1Pin, LOW);
@@ -52,19 +67,19 @@ void loop()
     }
   }
 
+  // Register new mmotion detection
   if (motionDetectorValuePrevious == LOW && motionDetectorValueCurrent == HIGH) {   // pin state change: LOW -> HIGH
     // set initial start time for counter
       if(motionStartTime == 0) {
         digitalWrite(led1Pin, HIGH);
         motionStartTime = loopStartTime;
         cycleStartTime = 0;
+    // Close timer for second hand movement
       } else if(motionDuration == 0) {
         motionDuration = loopStartTime - motionStartTime;
         tone(buzzerPin, map(motionDuration, 500, 100000, 10000, 50));
-        Serial.println("Values");
-        Serial.println(motionDuration);
-        Serial.println(map(motionDuration, 500, 10000, 10000, 50));
         cycleStartTime = loopStartTime;
+     // Reset state after third hand movement
       } else {
         motionStartTime = 0;
         motionDuration = 0;
